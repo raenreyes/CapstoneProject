54d04d276c1d6e8588947ec63605ff9d9710d339
raen.reyes <raen.reyes11@outlook.com>
2025-03-05 12:12:16 -0800

    yessuh

--- CapstoneProject/Program.cs
+++ CapstoneProject/Program.cs
@@ -11,7 +11,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
 builder.Services.AddRazorPages();
 builder.Services.AddDbContext<AppDbContext>(options =>
-               options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+               options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 builder.Services.Configure<StripeSettings>(builder.Configuration.GetSection("Stripe"));
 builder.Services.AddScoped<IProductService, CapstoneProject.Services.ProductService>();
 builder.Services.AddScoped<IOrderHeaderService, OrderheaderService>();
